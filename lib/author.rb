8bfed5b1e01eacef9ba5657f273ea8602d61604e
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-25 07:41:46 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -18,14 +18,14 @@ class Author
   
   
   def add_post_by_title(title)
-    post = Post.new(title)
-    add_post(post)
+    post_n = Post.new(title)
+    add_post(post_n)
   end
   
   def self.post_count
     posts = Post.all 
     count = 0 
-    posts.each do |post|
+    posts.each do |post_n|
       count += 1 
     end
   end
